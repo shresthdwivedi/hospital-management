f8bcedb9d46c48c3b8fd0db0c5062c1854cc420f
Shresth Dwivedi <shresth0109@gmail.com>
2024-07-14 15:39:55 +0530

    add message schema

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import { config } from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
+import { dbConneciton } from './database/dbConnection.js';
+import cloudinary from "cloudinary";
 
 const app = express();
 config({ path: './config/config.env' });
@@ -19,6 +21,15 @@ app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: '/tmp/',
 }));
+
+dbConneciton();
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
